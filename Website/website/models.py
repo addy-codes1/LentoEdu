408a829cb5fbd01c60596ada2a42d21c60502266
Shishir <114694079+Shishir-grez@users.noreply.github.com>
2023-10-08 11:41:13 +0530

    Sunil_Demo_Website

---
+++ Website/website/models.py
@@ -0,0 +1,20 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Responses(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    q1 = db.Column(db.Integer)
+    q2 = db.Column(db.Integer)
+    q3 = db.Column(db.Integer)
+    q4 = db.Column(db.Integer)
+    q5 = db.Column(db.Integer)
+    q6 = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    responses = db.relationship('Responses', backref='user')
